8a8786d8c35cb4306ce3e0c2bf94b5c3745508e1
AJ <ajisaacs@rogersengineering.net>
2019-04-04 08:53:25 -0400

    Removed border around textbox

--- ListFiles/Form1.Designer.cs
+++ ListFiles/Form1.Designer.cs
@@ -40,7 +40,9 @@
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.filesBox = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -52,14 +54,14 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.directoryBox, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.browseButton, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(694, 39);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(675, 39);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // label1
@@ -77,14 +79,14 @@
             this.directoryBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.directoryBox.Location = new System.Drawing.Point(77, 7);
             this.directoryBox.Name = "directoryBox";
-            this.directoryBox.Size = new System.Drawing.Size(508, 25);
+            this.directoryBox.Size = new System.Drawing.Size(489, 25);
             this.directoryBox.TabIndex = 1;
             this.directoryBox.TextChanged += new System.EventHandler(this.directoryBox_TextChanged);
             // 
             // browseButton
             // 
             this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.browseButton.Location = new System.Drawing.Point(591, 4);
+            this.browseButton.Location = new System.Drawing.Point(572, 4);
             this.browseButton.Name = "browseButton";
             this.browseButton.Size = new System.Drawing.Size(100, 30);
             this.browseButton.TabIndex = 2;
@@ -118,16 +120,16 @@
             // 
             this.searchPatternBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.searchPatternBox.Location = new System.Drawing.Point(416, 57);
+            this.searchPatternBox.Location = new System.Drawing.Point(422, 57);
             this.searchPatternBox.Name = "searchPatternBox";
-            this.searchPatternBox.Size = new System.Drawing.Size(290, 25);
+            this.searchPatternBox.Size = new System.Drawing.Size(265, 25);
             this.searchPatternBox.TabIndex = 6;
             this.searchPatternBox.TextChanged += new System.EventHandler(this.searchPatternBox_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(311, 60);
+            this.label2.Location = new System.Drawing.Point(317, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(99, 17);
             this.label2.TabIndex = 5;
@@ -161,7 +163,7 @@
             // 
             this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(634, 90);
+            this.linkLabel1.Location = new System.Drawing.Point(615, 87);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(72, 17);
             this.linkLabel1.TabIndex = 7;
@@ -171,28 +173,36 @@
             // 
             // filesBox
             // 
-            this.filesBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.filesBox.Location = new System.Drawing.Point(12, 113);
+            this.filesBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.filesBox.Location = new System.Drawing.Point(0, 114);
             this.filesBox.Name = "filesBox";
-            this.filesBox.Size = new System.Drawing.Size(694, 443);
+            this.filesBox.Size = new System.Drawing.Size(699, 492);
             this.filesBox.TabIndex = 8;
             this.filesBox.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Controls.Add(this.showFileExtensionsBox);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.showFullPathsBox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.searchPatternBox);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(699, 114);
+            this.panel1.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(718, 568);
+            this.ClientSize = new System.Drawing.Size(699, 606);
             this.Controls.Add(this.filesBox);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.searchPatternBox);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.showFullPathsBox);
-            this.Controls.Add(this.showFileExtensionsBox);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MinimumSize = new System.Drawing.Size(605, 356);
             this.Name = "Form1";
@@ -200,8 +210,9 @@
             this.Text = "ListFiles";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -219,6 +230,7 @@
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.RichTextBox filesBox;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
